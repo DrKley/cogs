8847e5afc420216799b8c7842d9e25ddb9839e13
DrKley <brian.v.r@hotmail.com>
2021-01-30 02:44:02 -0500

    Battle creation

---
+++ pokebattle/__init__.py
@@ -0,0 +1,5 @@
+from .pokebattle import PokeBattle
+
+
+def setup(bot):
+    bot.add_cog(PokeBattle())
--- pokecord/__init__.py
+++
@@ -1,7 +0,0 @@
-from .pokecord import Pokecord
-
-
-async def setup(bot):
-    cog = Pokecord(bot)
-    await cog.initalize()
-    bot.add_cog(cog)
